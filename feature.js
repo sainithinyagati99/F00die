def0a5ebc0b53c1490e71c69333753efe11bb0fd
sainithinyagati <36543416+sainithinyagati99@users.noreply.github.com>
2018-04-06 19:24:11 +0530

    Added all the Required Content and Files

---
+++ feature.js
@@ -0,0 +1,20 @@
+var a = document.getElementsByClassName("Morning1")[0];
+var b = document.getElementsByClassName("Afternoon1")[0];
+var c = document.getElementsByClassName("Night1")[0];
+var date = new Date();
+var day = date.getDay();
+switch (day) {
+    case 0://content comes here
+        break;
+    case 1://content comes here
+        break;
+    case 2://concent comes here
+        break;
+    case 3://content comes here
+        break;
+    case 4://content comes here
+        break;
+    case 5://content comes here
+        break;
+    case 6://content comes here
+}
